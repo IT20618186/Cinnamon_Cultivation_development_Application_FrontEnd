8156596c8ed376f50a891e66bc6cb0b64b93d717
IT20618186 <it20618186@my.sliit.lk>
2023-04-20 23:36:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import AdminHome from "./components/AdminHome/AdminHome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<Home/>}/>
+      <Route path="/AdminHome" exact element={<AdminHome/>}/>
+    </Routes>
   );
 }
 
